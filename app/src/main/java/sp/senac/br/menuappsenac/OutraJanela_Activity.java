a1caa4784d37fb4395fb19b00c6fd560ff0a2d37
lndsilva <lndsilva@hotmail.com>
2019-04-29 12:23:40 -0300

    Initial commit

---
+++ app/src/main/java/sp/senac/br/menuappsenac/OutraJanela_Activity.java
@@ -0,0 +1,13 @@
+package sp.senac.br.menuappsenac;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OutraJanela_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.outra_janela_layout);
+    }
+}
---
+++ app/src/main/java/sp/senac/br/menuappsenac/TerceiraJanelaActivity.java
@@ -0,0 +1,13 @@
+package sp.senac.br.menuappsenac;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TerceiraJanelaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.terceira_janela_layout);
+    }
+}
